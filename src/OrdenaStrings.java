37b4fd3e6e3745e87cb0a6de3958f96d473dda25
allanmateusk <allanmateusk@gmail.com>
2023-03-02 20:17:26 -0300

    quinto commit

---
+++ src/OrdenaStrings.java
@@ -0,0 +1,36 @@
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+import java.util.function.Consumer;
+
+public class OrdenaStrings {
+public static <T> void main(String[] args) {
+    List<String> palavras = new ArrayList<String>();
+    palavras.add("alura online");
+    palavras.add("editora casa do codigo");
+    palavras.add("caelum");
+    palavras.sort((s1, s2)->{
+            if(s1.length()<s2.length())
+            return -1;
+            if(s1.length()>s2.length())
+            return 1;
+            return 0;});
+           // palavras.sort((s1, s2)->Integer.compare(s1.length(),s2.length()));
+            palavras.sort(Comparator.comparing(s-> s.length()));
+
+    System.out.println(palavras);
+    Consumer<String>impressor = s-> System.out.println(s);
+    palavras.forEach(impressor);
+     palavras.forEach(s->System.out.println(s));
+}
+}
+
+class ComparadorPorTamanho implements Comparator<String>{
+    public int compare(String s1,String s2){
+        if(s1.length()<s2.length())
+        return -1;
+        if(s1.length()>s2.length())
+        return 1;
+        return 0;
+    }
+}
